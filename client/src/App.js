0b781c240c95bf34aefa994e4df78f539157b463
Michael Moreno <mmoreno2222@gmail.com>
2020-11-24 20:38:22 -0800

    authentication progress

--- client/src/App.js
+++ client/src/App.js
@@ -2,37 +2,31 @@ import React, { Component, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import API from "./utils/API";
+import Signup from "./components/Signup";
+import { Container } from "react-bootstrap";
 
 function App() {
-
-
   useEffect(() => {
-    API.getSongs().then(res => {
+    API.getSongs().then((res) => {
       console.log("yoooo, in APP.JS");
       console.log(res.data);
     });
-    API.getAllUsers().then( res => {
+    API.getAllUsers().then((res) => {
       console.log("...getting users from DB...");
       console.log(res.data);
-    })
+    });
   }, []);
 
-
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Signup />
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      <hr/>
-      <hr/>
-      
-    </div>
+    </Container>
   );
 }
-
 
 export default App;
---
+++ client/src/components/Signup.js
@@ -0,0 +1,45 @@
+import React, { useRef } from "react";
+import { Form, Card, Button } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+
+export default function Signup() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    signup(emailRef.current.value, passwordRef.current.value);
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          <Form>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirmation">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password-confirmation"
+                ref={passwordConfirmRef}
+                required
+              />
+            </Form.Group>
+            <Button type="submit">Sign Up</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
