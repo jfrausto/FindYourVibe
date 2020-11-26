f664d48eed213532be08e3b9093fe602469e2c9d
jessef <1319957+jfrausto@users.noreply.github.com>
2020-11-26 03:08:39 -0800

    console log the text area input on change; comment out api calls;

--- client/src/components/BlurbInput.js
+++ client/src/components/BlurbInput.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import Container from 'react-bootstrap/esm/Container';
 import TextareaCounter from 'react-textarea-counter';
 import './styles/BlurbInput.css';
@@ -6,15 +6,23 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ButtonGroup from './ButtonGroup';
 import DropdownMood from './DropdownMood';
+// import API from '../utils/API';
 
 // input group component that allows user input
-export default function BlurbInput(props) {
+export default function BlurbInput() {
+
+    const [TextAreaVal, setTextAreaVal] = useState("");
+    // const textAreaVal = useRef("What's on your mind?");
+    useEffect(() => {
+        console.log(TextAreaVal);
+    }, [TextAreaVal]);
+
     return (
         <>
             <Container className="mt-5">
                 <Row>
                     <Col xs={12} md={{span: 8, offset: 2}}>
-                        <TextareaCounter countLimit={140} rows={3}/>
+                        <TextareaCounter onChange={(e) => setTextAreaVal(e.target.value)} placeholder="What's on your mind?" countLimit={140} rows={3} />
                     </Col>
                 </Row>
                 <Row className="mt-2">
