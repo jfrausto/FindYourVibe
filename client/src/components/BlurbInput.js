40f3f280758a460c9d067f73f798f159d7b884b5
jessef <1319957+jfrausto@users.noreply.github.com>
2020-11-26 03:29:35 -0800

    console log noun finder results when you click on analyze button; add onClick handler in buttonGroup

--- client/src/components/BlurbInput.js
+++ client/src/components/BlurbInput.js
@@ -6,16 +6,29 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ButtonGroup from './ButtonGroup';
 import DropdownMood from './DropdownMood';
-// import API from '../utils/API';
+import API from '../utils/API';
 
 // input group component that allows user input
 export default function BlurbInput() {
 
     const [TextAreaVal, setTextAreaVal] = useState("");
-    // const textAreaVal = useRef("What's on your mind?");
-    useEffect(() => {
-        console.log(TextAreaVal);
-    }, [TextAreaVal]);
+    // useEffect(() => {
+    //     console.log(TextAreaVal);
+    // }, [TextAreaVal]);
+
+    const handleButtonClick = (e) => {
+        const buttonPress = e.target.innerText;
+        console.log(buttonPress);
+        // do not call the api on an empty string
+        if (TextAreaVal === "") return;
+        if(buttonPress === "Analyze"){
+            API.getNouns(TextAreaVal).then( (res) => {
+                console.log(res.data);
+            })
+        } else {
+            console.log("post button click!");
+        }
+    }
 
     return (
         <>
@@ -26,7 +39,7 @@ export default function BlurbInput() {
                     </Col>
                 </Row>
                 <Row className="mt-2">
-                    <ButtonGroup />
+                    <ButtonGroup handleButtonClick={handleButtonClick}/>
                     <DropdownMood />
                 </Row>
             </Container>
