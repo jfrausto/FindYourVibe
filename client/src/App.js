a45cb2ca8ec4801716d969f19f720d806ca02dc8
Michael Moreno <mmoreno2222@gmail.com>
2020-11-27 13:33:51 -0800

    signup rendering onto page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import API from "./utils/API";
+import Signup from "./components/Signup";
+import { Container } from "react-bootstrap";
 // import useNounFinder from "./utils/Hooks/NounFinderFolder/NounFinder";
 import LandingSplash from "./components/LandingSplash";
 import BlurbInput from "./components/BlurbInput";
 
 function App() {
-
   useEffect(() => {
     // API.getSongs().then(res => {
     //   console.log("yoooo, in APP.JS");
@@ -22,17 +23,23 @@ function App() {
     // });
   }, []);
 
-
   return (
     <div className="App">
       {/* Navigation  or other stuff can go here */}
-      <LandingSplash/>
+      <LandingSplash />
       <BlurbInput />
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
       {/* we might not even want this landing splash thing in the future */}
       {/* perhaps some buttons to continue */}
     </div>
   );
 }
-
 
 export default App;
