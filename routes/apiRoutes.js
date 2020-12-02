989064346bb51337bbd1e547168603cc8a23350e
Michael Moreno <mmoreno2222@gmail.com>
2020-12-01 21:48:21 -0800

    adding server features

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,10 @@
 const Genius = require("genius-lyrics");
-const Token = "GjZdsQku4iZ7k9QRw0v3g2PRBRe6zrAe3NzPLBqdA-fBEFC_u1DbxuFF5FZvSxPL";
+const Token = process.env.API_GENIUS;
 const Client = new Genius.Client(Token);
 const router = require("express").Router();
-var createNounfinder = require('nounfinder');
+var createNounfinder = require("nounfinder");
 var nounFinder = createNounfinder({
-  wordnikAPIKey: 'v0e8pfefcqx3zr3o0ouhz12c48noz2nibg3drs3k1yzb9urvt'
+  wordnikAPIKey: process.env.API_WORDNIK,
 });
 const db = require("../models");
 // declare the song results here
@@ -23,22 +23,22 @@ const wait = (time) =>
     }, time);
   });
 const test = async (stringifiedNounsQuery) => {
-//   const startTime = Date.now();
+  //   const startTime = Date.now();
   console.log(`Genius-Lyrics v${Genius.Version}`);
   const searches = await Client.songs.search(stringifiedNounsQuery);
-//   return searches;
-//   Lets see the first song
-//   console.log("length of songs array: "+ searches.length)
+  //   return searches;
+  //   Lets see the first song
+  //   console.log("length of songs array: "+ searches.length)
   firstSong = searches[0];
   // await wait(100);
   // const lyrics1 = await firstSong.lyrics();
   // await wait(100);
-  secondSong = searches[1]; 
+  secondSong = searches[1];
   // const lyrics2 = await secondSong.lyrics();
   // await wait(100);
   thirdSong = searches[2];
   // const lyrics3 = await thirdSong.lyrics();
-  
+
   // const songAndLyricPairs = [
   //   { id: 1,
   //     song: firstSong,
@@ -57,16 +57,15 @@ const test = async (stringifiedNounsQuery) => {
   // return songAndLyricPairs;
   const songPool = [firstSong, secondSong, thirdSong];
   return songPool;
-//   console.log("About the Song:\n", firstSong, "\n");
+  //   console.log("About the Song:\n", firstSong, "\n");
   // await wait(1000);
   // Ok lets get the lyrics
   // const lyrics = await firstSong.lyrics();
-//   console.log("Lyrics of the Song:\n", lyrics, "\n");
+  //   console.log("Lyrics of the Song:\n", lyrics, "\n");
   // return lyrics;
-//   await wait(1000);
+  //   await wait(1000);
 };
 // test();
-
 
 // ---------------GET LYRICS FUNCTION
 const getLyrics = async (chosenSong) => {
@@ -81,66 +80,66 @@ const getLyrics = async (chosenSong) => {
     throw error;
   }
   return lyricRes;
-}
+};
 
 // ! -------------------- Define API routes here
 // * this matches with /api/songs
 // ! remember /api/ is implied in our server.js file
 router.get("/songs/:lyrics", (req, res) => {
-    console.log("... inside router.get('/songs/:lyrics')...");
-    // array was 'stringified by axios, now we remove the commas
-    let stringifiedArray = req.params.lyrics;
-    // replace the commas with a space
-    stringifiedArray = stringifiedArray.replace(/,/g, ' ');
-    console.log(stringifiedArray);
-    test(stringifiedArray).then( data => {
-      console.log("successfully returned song pool!");
-      res.send(data);
-    })
-    
+  console.log("... inside router.get('/songs/:lyrics')...");
+  // array was 'stringified by axios, now we remove the commas
+  let stringifiedArray = req.params.lyrics;
+  // replace the commas with a space
+  stringifiedArray = stringifiedArray.replace(/,/g, " ");
+  console.log(stringifiedArray);
+  test(stringifiedArray).then((data) => {
+    console.log("successfully returned song pool!");
+    res.send(data);
+  });
 });
 
 // this route gets the lyrics of a particular song
 // uses param option inside API.js axios request
 router.get("/lyrics/:cardID", (req, res) => {
-    console.log("...inside router.get('/lyrics/:songObj')...");
-    // let singleSong = JSON.parse(req.query.songObj);
-    // console.log(singleSong.title);
-    const selectedCard = req.params.cardID;
-    let singleSong;
-    console.log(selectedCard);
-    switch (selectedCard) {
-      case "1":
-        singleSong = firstSong;
-        break;
-      case "2":
-        singleSong = secondSong;
-        break;
-      default:
-        singleSong = thirdSong;
-        break;
-    }
+  console.log("...inside router.get('/lyrics/:songObj')...");
+  // let singleSong = JSON.parse(req.query.songObj);
+  // console.log(singleSong.title);
+  const selectedCard = req.params.cardID;
+  let singleSong;
+  console.log(selectedCard);
+  switch (selectedCard) {
+    case "1":
+      singleSong = firstSong;
+      break;
+    case "2":
+      singleSong = secondSong;
+      break;
+    default:
+      singleSong = thirdSong;
+      break;
+  }
 
-    getLyrics(singleSong).then( (data) => {
-      console.log(`successfully got lyrics from the backend!!`);
-      res.send(data);
-    });
+  getLyrics(singleSong).then((data) => {
+    console.log(`successfully got lyrics from the backend!!`);
+    res.send(data);
+  });
 });
 
 // FIND ALL USERS ROUTE
 router.get("/users", (req, res) => {
   console.log("...finding users...");
-  db.User.find( {} ).then( (data) => {
-    console.log("found all users");
-    res.json(data);
-  })
-  .catch( (err) => res.status(422).json(err));
+  db.User.find({})
+    .then((data) => {
+      console.log("found all users");
+      res.json(data);
+    })
+    .catch((err) => res.status(422).json(err));
 });
 
 // CALL NOUN FINDER API AND RETURNS AN ARRAY
 router.get("/nouns/:words", (req, res) => {
   console.log("... inside router.get('/nouns/:words')");
-  const text = req.params.words
+  const text = req.params.words;
   nounFinder.getNounsFromText(text, function done(error, nouns) {
     if (error) throw error;
     console.log(`Filtered nouns: ${nouns}`);
@@ -156,12 +155,13 @@ router.post("/postBlurb", (req, res) => {
   console.log(update);
   // find and update john connor for now
   // update object is found in BlurbInput.js
-  db.User.findOneAndUpdate({firstName: "John", lastName: "Connor"}, update)
-  .then( (data) => {
+  db.User.findOneAndUpdate(
+    { firstName: "John", lastName: "Connor" },
+    update
+  ).then((data) => {
     console.log("updated one record!");
     res.json(data);
-  })
-
-})
+  });
+});
 
 module.exports = router;
