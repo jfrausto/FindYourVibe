4a6c653b797083eba447781080e83419e0cce256
JOCECODE <ulah57@gmail.com>
2020-12-09 21:49:30 -0800

    CREATE IMG CARASOUEL;

--- client/src/components/Steps.js
+++ client/src/components/Steps.js
@@ -1,21 +1,65 @@
-import React from 'react'
-import Container from "react-bootstrap/Container";
-import "./styles/About.css";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import ImageCarousel from "./ImageCarousel";
+import React from 'react';
+import "./styles/Steps.css";
+
 export default function Steps() {
  return (
-  <>
-  <ImageCarousel/>
-  <Container className="aboutBackground px-0" fluid>
-   <Row>
-    <Col md="12">
-     <img  className="img-fluid welcomeImage" alt="profile" src="https://i.imgur.com/WIZ0EXQ.png" width="640px" height="1px"/>
-    </Col>
-   </Row>
-    </Container>
-   
+   <>
+  <div className="stepsContainer">  
+    <div className="imgCarousel">
+      <div className="steps step1">
+       <div className="textContent">
+         <h1>
+           Step 1
+         </h1>
+         <p>The first thing that you have to do is read this!</p>
+       </div>
+       </div>
+      <div className="steps step2">
+       <div className="textContent">
+         <h1>
+           Step 2
+         </h1>
+         <p>The first thing that you have to do is read this!</p>
+       </div>
+       </div>
+      <div className="steps step3">
+       <div className="textContent">
+         <h1>
+           Step 3
+         </h1>
+         <p>The first thing that you have to do is read this!</p>
+       </div>
+       </div>
+      <div className="steps step4">
+       <div className="textContent">
+         <h1>
+           Step 4
+         </h1>
+         <p>The first thing that you have to do is read this!</p>
+       </div>
+       </div>
+      <div className="steps step5">
+       <div className="textContent">
+         <h1>
+           Step 5
+         </h1>
+         <p>The first thing that you have to do is read this!</p>
+       </div>
+       </div>
+      <div className="steps step6">
+       <div className="textContent">
+         <h1>
+           Step 6
+         </h1>
+         <p>The first thing that you have to do is read this!</p>
+       </div>
+       </div>
+    </div>
+    <div class="buttons">
+      <div id="prev"></div>
+      <div id="next"></div>
+    </div>
+  </div>
   </>
  )
 }
--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Header";
+import Steps from "../Steps";
 import "../styles/About.css";
 
 export default function Homepage() {
   return (
     <>
     <Header/>
+    <Steps/>
     </>
   );
 }
