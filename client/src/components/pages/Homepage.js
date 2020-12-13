c9bce55d320dec7cac17cc8270853a7936a41ed8
JOCECODE <ulah57@gmail.com>
2020-12-13 01:51:47 -0800

    CREATE IMAGE VIBEE;

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../Header";
-import Steps from "../Steps";
 // import Container from "react-bootstrap/Container";
 
 export default function Homepage() {
