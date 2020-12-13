7126f9f9a02e0a52383f77fe392989757b0ec628
JOCECODE <ulah57@gmail.com>
2020-12-12 19:29:32 -0800

    ADD SVG PATH FILES;

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Header from "../Header";
 import Steps from "../Steps";
-import "../styles/About.css";
 // import Container from "react-bootstrap/Container";
 
 export default function Homepage() {
   return (
     <>
     <Header/>
-    <Steps/>
+    {/* <Steps/> */}
     </>
   );
 }
