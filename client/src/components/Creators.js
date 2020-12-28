125b1e1c43563ee3e70630879d1fd2f5a37d0090
JOCECODE <ulah57@gmail.com>
2020-12-27 16:08:37 -0800

    CREATE CREATORS COMPONENT;

---
+++ client/src/components/Creators.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Creators() {
+ return (
+  <>
+   <h1 style={{color: "white"}}>Hello Darkness</h1>
+  </>
+ )
+}
--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from "../Navbar";
 import Steps from "../Steps";
+import Creators from "../Creators";
 
 export default function About() {
  return (
   <>
     <Navbar />
     {/* <Steps/> */}
-    
+    <Creators/>
   </>
  )
 }
