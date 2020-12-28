6b63ccb46d4d28e600cdd6998c225f6e8965fd7d
JOCECODE <ulah57@gmail.com>
2020-12-27 16:33:47 -0800

    CREATE CREATORS LAYOUT;

--- client/src/components/Creators.js
+++ client/src/components/Creators.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./styles/Creators.css";
+
+
 
 export default function Creators() {
+
  return (
   <>
-   <h1 style={{color: "white"}}>Hello Darkness</h1>
+   <div className="testimonials">
+    <div className="swiper-container">
+     <div className="swiper-wrapper">
+      <div className="swiper-slide">
+       <div className="page">
+        <div className="layer"></div>
+         <div className="content">
+          <p style={{color: "black"}}>LOREM SHIT I AM WRITING RANDOM STUFF HERE SO BARE WITH ME IF THIS SEEMS TO BE OUT OF THE ORDINARY FOR YOU</p>
+          <div className="img-Bx">
+           <img height="100px" width="100px" src="https://i.imgur.com/hCVt35Q.jpg"/>
+          </div>
+          <div className="details">
+           <h2 style={{color: "black"}}>Someone Famous<br/><span>Website Designer</span></h2>
+          </div>
+        </div>
+       </div>
+      </div>
+     </div>
+    </div>
+   </div>
+   
   </>
  )
 }
