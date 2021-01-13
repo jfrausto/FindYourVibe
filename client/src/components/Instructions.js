036d591cfaf7b114a16cc14b60fc1b604f2bd001
JOCECODE <ulah57@gmail.com>
2021-01-12 21:38:03 -0800

    FORMAT SKELETON ABOUT US PAGE INCLUDE LEFT, MIDDLE, RIGHT COLUMN; CREATE HERO IMAGE COMPONENT;

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -3,11 +3,40 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./styles/Instructions.css";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 export default function Instructions() {
  return (
   <>
+  {/* <Container fluid>
+   
+   HERO IMAGE
+   <Row >
+    <Col  style={{overflow: "hidden"}} className="heroImage">
+    <Image src="https://www.setaswall.com/wp-content/uploads/2017/03/Artistic-Landscape-4K-Wallpaper-3840x2160.jpg" fluid />
+    </Col>
+   </Row>
+  </Container> */}
+
+  <Jumbotron id="jumboTronAbout">
+  <Container >
+    <h1 className="fontColor">Vibee</h1>
+    <p className="fontColor">
+      This is a modified jumbotron that occupies the entire horizontal space of
+      its parent.
+    </p>
+  </Container>
+</Jumbotron>
+
+
   <Container>
+   <Row>
+    <Col>
+    
+    </Col>
+   </Row>
+
+
    <Row>
 
     {/* LEFT COLUMN */}
@@ -126,7 +155,7 @@ export default function Instructions() {
     
     </Col>
 
-    
+
    </Row>
   </Container>
 
