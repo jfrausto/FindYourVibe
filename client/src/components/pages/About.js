5e790a3fcf1da1d5f53f91332a9adfd1c9ce173c
JOCECODE <ulah57@gmail.com>
2021-01-12 20:05:24 -0800

    CREATE INSTRUCTIONS COMPONENT; CREATE INSTRUCTIONS.CSS; INCORPORATE INSTRUCTIONS.JS COMPONENT TO ABOUT.JS COMPONENT; CREATE LAYOUT FOR ABOUT US PAGE; CREATE MIDDLE COL SKELETON;

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from "../Navbar";
 import Creators from "../Creators";
+import Instructions from "../Instructions";
 
 export default function About() {
  return (
   <>
     <Navbar />
+    <Instructions/>
     <Creators />
   </>
  )
