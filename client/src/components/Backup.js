08b690415d60dbb49127132b93c33beca10b5208
JOCECODE <ulah57@gmail.com>
2021-01-21 19:47:22 -0800

    CREATE INSTRUCTIONS COMPONENT; RENAME Instructions.js to HeroImage.js;

---
+++ client/src/components/Backup.js
@@ -0,0 +1,130 @@
+//! INSTRUCTIONS FIRST WAY SETTING IT UP
+<Container>
+<Row>
+ <Col>
+ 
+ </Col>
+</Row>
+
+
+<Row>
+
+ {/* LEFT COLUMN */}
+ <Col>
+ 
+ </Col>
+
+
+  {/* MIDDLE COLUMN */}
+ <Col>
+ 
+{/* HOW IT WORKS */}
+<Col xs={12}>
+ <h1>
+  HOW IT WORKS
+ </h1>
+ </Col>
+ <Col xs={12}>
+ <p>
+  STEP
+ </p>
+ </Col>
+ <Col className="ohOne" xs={12}>
+ <h2>
+  01
+ </h2>
+ </Col>
+ <Col className="divider" xs={12}>
+ <h3 style={{fontSize: "14px"}}>
+  O
+
+  O
+  
+  O
+
+  O
+
+  O
+
+  O
+  
+  O
+  
+  O
+  
+  O
+  
+  O
+  
+  O
+  
+  O
+  
+  O
+ </h3>
+ </Col>
+ 
+ <Col>
+ <Col xs={12}>
+ <p>
+  STEP
+ </p>
+ </Col>
+ <Col xs={12}>
+ <h2>
+  02
+ </h2>
+ </Col>
+ <Col className="divider divider2" xs={12}>
+ <h3 style={{fontSize: "14px", marginBottom: "40%"}}>
+  O
+
+  O
+  
+  O
+
+  O
+
+  O
+
+  O
+  
+  O
+  
+  O
+  
+  O
+  
+  O
+  
+  O
+  
+  O
+  
+  O
+ </h3>
+ </Col>
+ </Col>
+ <Col xs={12}>
+ <Col className="step3Col" xs={12}>
+ <p>
+  STEP
+ </p>
+ </Col>
+ <Col xs={12}>
+ <h2>
+  03
+ </h2>
+ </Col>
+ </Col>
+ </Col>
+
+
+  {/* RIGHT COLUMN */}
+ <Col>
+ 
+ </Col>
+
+
+</Row>
+</Container>
--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from "../Navbar";
 import Creators from "../Creators";
+import HeroImage from "../HeroImage";
 import Instructions from "../Instructions";
+
 
 export default function About() {
  return (
   <>
     <Navbar />
+    <HeroImage/>
     <Instructions/>
     <Creators />
   </>
