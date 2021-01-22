5ad43ca09a9dea35f7c6fcb4a7df97e7e6dbd338
JOCECODE <ulah57@gmail.com>
2021-01-21 20:13:13 -0800

    SAVE TO SWITCH BRANCH

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import "react-bootstrap/Container";
+import "react-bootstrap/Row";
+import "react-bootstrap/Col";
 
 export default function Instructions() {
  return (
   <>
-   <h1>
-    HELLO WORLD WELCOME TO THE JUNGLE
-   </h1>
+  <Container>
+   <Row>
+    <Col xs={8}>
+    
+    </Col>
+    <Col xs={4}>
+
+    </Col>
+   </Row>
+  </Container>
   </>
  )
 }
