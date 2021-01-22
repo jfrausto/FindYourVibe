b602b9778eebaa4461443613c6794cfbb16ead70
jessef <1319957+jfrausto@users.noreply.github.com>
2021-01-21 20:07:09 -0800

    toast component now shows a 'no results' error

--- client/src/components/PostToastError.js
+++ client/src/components/PostToastError.js
@@ -4,18 +4,42 @@ import Col from 'react-bootstrap/Col';
 
 export default function PostToastError({ showToast }) {
 
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState({
+    emptySong: false,
+    noResults: false
+  });
 
   useEffect(() => {
-    setShow(showToast);
-  }, [showToast]);
+    setShow({
+      emptySong: showToast.emptySong,
+      noResults: showToast.noResults
+    });
+  }, [showToast.emptySong, showToast.noResults]);
 
   return (
     <>
       <Col xs={12}
       >
-          <Toast animation={true} className="mx-auto" onClose={() => setShow(false)} show={show} delay={1500} autohide>
+          <Toast 
+            animation={true} 
+            className="mx-auto" 
+            onClose={() => setShow({emptySong: false, noResults: false})} 
+            show={show.emptySong} 
+            delay={1500} 
+            autohide
+          >
             <Toast.Body className="alert-msg">select a song and/or write your thoughts!</Toast.Body>
+          </Toast>
+          
+          <Toast 
+            animation={true} 
+            className="mx-auto" 
+            onClose={() => setShow({emptySong: false, noResults: false})} 
+            show={show.noResults} 
+            delay={1500} 
+            autohide
+          >
+            <Toast.Body className="alert-msg">oh! no songs were found!</Toast.Body>
           </Toast>
       </Col>
     </>
